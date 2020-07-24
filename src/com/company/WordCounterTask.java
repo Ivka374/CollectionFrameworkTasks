814fa5aab9a0ec359896f1d7ee8ebca52fd6ed90
Ivka374 <ivka@dsoft-bg.com>
2020-07-24 18:01:29 +0300

    Finished the WordCounterTask

--- src/com/company/WordCounterTask.java
+++ src/com/company/WordCounterTask.java
@@ -1,4 +1,24 @@
 package com.company;
 
+import java.util.*;
+
 public class WordCounterTask {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        String[] words = scanner.nextLine().toLowerCase().split(" ");
+
+        Map<String, Integer> wordCount = new HashMap<>();
+        for (String word : words) {
+            wordCount.merge(word, 1, Integer::sum);
+        }
+
+        printWords(wordCount);
+    }
+
+    private static void printWords(Map<String, Integer> wordAmount){
+        for (String word : wordAmount.keySet()) {
+            System.out.println(word + " " + wordAmount.get(word));
+        }
+    }
 }
