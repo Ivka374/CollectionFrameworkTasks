b238c57d07c2fdca9cc7e6d45c082282fc1311bb
Ivka374 <ivka@dsoft-bg.com>
2020-07-24 17:45:40 +0300

    Done with Sherlock Holmes task

--- src/com/company/DetectiveTask.java
+++ src/com/company/DetectiveTask.java
@@ -5,11 +5,11 @@ import java.util.*;
 public class DetectiveTask {
     public static void main(String[] args) {
         Map<Character, Integer> firstWordLetters = new HashMap<>();
-        Map<Character, Integer> secondWordLetters = new HashMap<>();
+        int differentLetters = 0;
 
         Scanner scanner = new Scanner(System.in);
-        String firstWord = scanner.nextLine();
-        String secondWord = scanner.nextLine();
+        String firstWord = scanner.nextLine().toLowerCase();
+        String secondWord = scanner.nextLine().toLowerCase();
 
         for (int i = 0; i < firstWord.length(); i++){
             char currentLetter = firstWord.charAt(i);
@@ -17,7 +17,21 @@ public class DetectiveTask {
         }
         for (int i = 0; i < secondWord.length(); i++){
             char currentLetter = secondWord.charAt(i);
-            secondWordLetters.merge(currentLetter, 1, Integer::sum);
+            if (firstWordLetters.get(currentLetter) != null){
+                firstWordLetters.put(currentLetter, firstWordLetters.get(currentLetter) - 1);
+                if (firstWordLetters.get(currentLetter) == 0)firstWordLetters.remove(currentLetter);
+            } else {
+                differentLetters++;
+            }
         }
+
+        if (!firstWordLetters.isEmpty()) {
+            for (Character i : firstWordLetters.keySet()) {
+                differentLetters += firstWordLetters.get(i);
+            }
+            firstWordLetters.clear();
+        }
+
+        System.out.println(differentLetters);
     }
 }
