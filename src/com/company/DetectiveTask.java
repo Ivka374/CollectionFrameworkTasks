9bcc5365f0ecb32773787509349e89b66c3f4884
Ivka374 <ivka@dsoft-bg.com>
2020-07-24 17:10:35 +0300

    Input on the Sherlock Holmes problems

--- src/com/company/DetectiveTask.java
+++ src/com/company/DetectiveTask.java
@@ -1,4 +1,23 @@
 package com.company;
 
+import java.util.*;
+
 public class DetectiveTask {
+    public static void main(String[] args) {
+        Map<Character, Integer> firstWordLetters = new HashMap<>();
+        Map<Character, Integer> secondWordLetters = new HashMap<>();
+
+        Scanner scanner = new Scanner(System.in);
+        String firstWord = scanner.nextLine();
+        String secondWord = scanner.nextLine();
+
+        for (int i = 0; i < firstWord.length(); i++){
+            char currentLetter = firstWord.charAt(i);
+            firstWordLetters.merge(currentLetter, 1, Integer::sum);
+        }
+        for (int i = 0; i < secondWord.length(); i++){
+            char currentLetter = secondWord.charAt(i);
+            secondWordLetters.merge(currentLetter, 1, Integer::sum);
+        }
+    }
 }
--- src/com/company/KidnapperTask.java
+++ src/com/company/KidnapperTask.java
@@ -1,7 +1,12 @@
 package com.company;
 
+import java.util.LinkedList;
+import java.util.List;
+
 public class KidnapperTask {
     public static void main(String[] args) {
-        
+        List<String> availableWords = new LinkedList<>();
+        List<String> neededWords = new LinkedList<>();
+
     }
 }
